a6d144e7013a20891ffe0ee29a6cfda0c36e51e3
psong1 <paulsong29@gmail.com>
2023-03-20 00:23:39 -0400

    wrote seeds and db sql and tested mysql to show tables

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,19 +1,31 @@
-CREATE TABLE department (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(30)
-);
+INSERT INTO department
+    (name)
+VALUES 
+    ('Sales'),
+    ('Engineering'),
+    ('Finance'),
+    ('Legal');
 
-CREATE TABLE role (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(30),
-    salary DECIMAL,
-    department_id INT
-);
+INSERT INTO role
+    (title, salary, department_id)
+VALUES
+    ('Sales Lead', 100000, 1),
+    ('Salesperson', 80000, 2),
+    ('Lead Engineer', 150000, 3),
+    ('Software Engineer', 120000, 4),
+    ('Account Manager', 160000, 5),
+    ('Accountant', 125000, 6),
+    ('Legal Team Lead', 250000, 7),
+    ('Lawyer', 190000, 8);
 
-CREATE TABLE employee (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
-    role_id INT,
-    manager_id INT
-);
+INSERT INTO employee
+    (first_name, last_name, role_id, manager_id)
+VALUES 
+    ('John', 'Doe', 1, NULL),
+    ('Mike', 'Chan', 2, 1),
+    ('Ashley', 'Rodriguez', 3, NULL),
+    ('Kevin', 'Tupik', 4, 3),
+    ('Kunal', 'Singh', 5, NULL),
+    ('Malia', 'Brown', 6, 5),
+    ('Sarah', 'Lourd', 7, NULL),
+    ('Tom', 'Allen', 8, 7);
